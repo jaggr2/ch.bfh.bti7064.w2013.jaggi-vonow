a7a5f8379161a8505e44c8acb0db6a1297f24afc
Roger Jaggi <roger.jaggi@gmail.com>
2014-01-17 14:37:01 +0100

    commit aktueller Stand

--- src/NeaState.java
+++ src/NeaState.java
@@ -1,5 +1,9 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Copyright 2014 blastbeat syndicate gmbh
@@ -9,51 +13,145 @@ import java.util.List;
  */
 public abstract class NeaState {
 
+
+
+    private List<NeaState> validFollingStateLeft = new ArrayList<NeaState>();
+    private List<NeaState> validFollingStateRight = new ArrayList<NeaState>();
+
     public String getName() {
         return name;
     }
 
     private String name;
 
+    public List<NeaState> getValidFollingStateLeft() {
+        return validFollingStateLeft;
+    }
+
+    public List<NeaState> getValidFollingStateRight() {
+        return validFollingStateRight;
+    }
+
+    public void addValidFollingStateOneLeft(NeaState state) {
+        Objects.requireNonNull(state);
+
+        this.validFollingStateLeft.add(state);
+    }
+
+    public void addValidFollingStateOneRight(NeaState state) {
+        Objects.requireNonNull(state);
+
+        this.validFollingStateRight.add(state);
+    }
+
+    public String getListOfFollingStatesLeft() {
+        StringBuilder sb = new StringBuilder();
+        for(NeaState state : this.getValidFollingStateLeft()) {
+            sb.append(state.getName() + ",");
+        }
+        return sb.toString();
+    }
+
+    public String getListOfFollingStatesRight() {
+        StringBuilder sb = new StringBuilder();
+        for(NeaState state : this.getValidFollingStateRight()) {
+            sb.append(state.getName() + ",");
+        }
+        return sb.toString();
+    }
+
     public NeaState(String name) {
         this.name = name;
     }
 
-    public String getContainingToken(String term, char openingChar, char closingChar) {
-        int[] pos = findSourroundingChars(term, openingChar, closingChar);
+    public String getContainingToken(String term, char openingChar, char closingChar) throws ParseException {
+        int[] pos = findSourroundingChars(term, openingChar, closingChar, 0);
         if(pos.length > 0) {
-            return term.substring(pos[0],pos[1]);
+            return term.substring(pos[0] + 1,pos[1]).trim();
         }
         else {
             return null;
         }
     }
 
-    public int[] findSourroundingChars(String term, char openingChar, char closingChar) {
+    public int[] findSourroundingChars(String term, char openingChar, char closingChar, int startPos) throws ParseException {
         int stack = 0;
         int posFirstChar = -1;
         int posLastChar = -1;
-        for(int i = 0; i < term.length(); i++) {
+        for(int i = startPos; i < term.length(); i++) {
             if(term.charAt(i) == openingChar) {
                 stack += 1;
-                if(posFirstChar != -1) {
+                if(posFirstChar == -1) {
                     posFirstChar = i;
                 }
             }
             if(term.charAt(i) == closingChar) {
                 stack -= 1;
                 if(stack == 0) {
-                    posLastChar = i;
+                    if(posFirstChar != -1) {
+                        return new int[]{posFirstChar, i};
+                    }
+                    else {
+                        throw new ParseException("Closing Char " + closingChar + " ohne öffnendes Char " + openingChar + " in " + term + " gefunden!", 0);
+                    }
                 }
             }
         }
 
-        if(posFirstChar != -1 && posLastChar != -1) {
-            return new int[]{posFirstChar, posLastChar};
+        return new int[]{};
+    }
+
+    public List<String> splitListSave(String term, String splitToken, int startPos) {
+        int stack = 0;
+        List<String> result = new ArrayList<String>();
+        int lasPos = 0;
+        int correctCount = 0;
+
+        for(int i = startPos; i < term.length(); i++) {
+            if(term.charAt(i) == '(' || term.charAt(i) == '[') {
+                stack += 1;
+            }
+            if(term.charAt(i) == ')' || term.charAt(i) == ']') {
+                stack -= 1;
+            }
+            if(term.charAt(i) == splitToken.charAt(correctCount)) {
+
+                if((correctCount + 1) == splitToken.length()) {
+                    result.add(term.substring(lasPos, i - (correctCount)).trim());
+                    lasPos = i + 1;
+                }
+                else {
+                    correctCount += 1;
+                }
+
+            }
+            else {
+                correctCount = 0;
+            }
+
         }
-        else {
-            return new int[]{};
+
+        result.add(term.substring(lasPos).trim());
+
+        return (stack == 0 ? result : new ArrayList<String>());
+    }
+
+    public boolean parseWithOneOfFollowingStatesLeft(String containingToken) throws ParseException {
+        for(NeaState state : this.getValidFollingStateLeft()) {
+            if(state.parseToken(containingToken)) {
+                return true;
+            }
         }
+        throw new ParseException("None of the folling States [" + getListOfFollingStatesLeft() + "] of State [" + this.name + "] returned true for: " + containingToken ,0);
+    }
+
+    public boolean parseWithOneOfFollowingStatesRight(String containingToken) throws ParseException {
+        for(NeaState state : this.getValidFollingStateRight()) {
+            if(state.parseToken(containingToken)) {
+                return true;
+            }
+        }
+        throw new ParseException("None of the folling States [" + getListOfFollingStatesRight() + "] of State [" + this.name + "] returned true for: " + containingToken ,0);
     }
 
     /**
@@ -61,22 +159,8 @@ public abstract class NeaState {
      * @param token the token
      * @return returns true if valid
      */
-    public abstract boolean parseToken(String token);
-    /*{
-        /*
-        Boolean isChoice
-        for(int i = 0; i < term.length(); i++) {
-            if(term.charAt(i) == '[') {
+    public abstract boolean parseToken(String token) throws ParseException;
 
-            }
-
-
-        }
-
-        if(token)
-
-
-    } */
 
 
 }
--- src/PrologParser.java
+++ src/PrologParser.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
 
 public class PrologParser {
 
-	public static void main (String args[]) throws IOException {
+	public static void main (String args[]) throws IOException, ParseException {
 
         if(args.length != 1) {
             System.out.println("ERROR: Bitte als einzigster Kommandozeilenparameter eine Prolog-Datei angeben.");
@@ -29,6 +31,21 @@ public class PrologParser {
                     continue;
                 }
 
+                Integer start = 0;
+                Integer end = line.indexOf("/*");
+                String front;
+                while(end >= 0) {
+                    front = line.substring(start, end);
+
+                    start = line.indexOf("*/", end);
+                    if(start <= 0) {
+                        throw new ParseException("Unclosing /* comment",0);
+                    }
+                    line = front + line.substring(start + 2);
+                    end = line.indexOf("/*", start);
+                }
+                /* sample moves */
+
                 sb.append(line);
                 //sb.append(System.lineSeparator());
                 line = br.readLine();
@@ -38,39 +55,6 @@ public class PrologParser {
             br.close();
         }
 
-        // define the NEA Automat
-        NeaState kostante = new NeaState("Konstante") {
-            @Override
-            public boolean parseToken(String token) {
-                return token.matches("[[a-z]{1}[a-zA-Z0-9_]*|[0-9]+]");
-            }
-        };
-
-        NeaState variable = new NeaState("Variable") {
-            @Override
-            public boolean parseToken(String token) {
-                return token.matches("[A-Z_][a-zA-Z0-9_]*");
-            }
-        };
-
-        NeaState struktur = new NeaState("Struktur") {
-            @Override
-            public boolean parseToken(String token) {
-                if(!token.matches("[a-z_]+\\(.+")) {
-                    return false;
-                }
-
-                String containingToken = getContainingToken(token, '(',')');
-                if(containingToken == null) {
-                    return false;
-                }
-
-
-                return token.matches("[A-Z_][a-zA-Z0-9_]*");
-
-
-            }
-        };
 
         /*
         NeaState konstante = new NeaState("Konstante", "[[a-z]{1}[a-zA-Z0-9_]*|[0-9]+]{1}");
@@ -80,25 +64,126 @@ public class PrologParser {
         NeaState liste = new NeaState("Liste", "\\[\\Anweisung*\\]");
         NeaState zuweisung = new NeaState("Zuweisung", "[\\Strukur|\\Variable|\\Konstante]:-\\Anweisung");
         NeaState prologterm = new NeaState("Term", "[\\Struktur|\\Zuweisung]{1}\\.");
+        */
 
-        struktur.addAllowedState(anweisung);
-        anweisung.addAllowedState(variable);
-        anweisung.addAllowedState(konstante);
-        anweisung.addAllowedState(struktur);
-        anweisung.addAllowedState(liste);
-        liste.addAllowedState(anweisung);
-        zuweisung.addAllowedState(struktur);
-        zuweisung.addAllowedState(variable);
-        zuweisung.addAllowedState(konstante);
-        zuweisung.addAllowedState(anweisung);
-        prologterm.addAllowedState(struktur);
-        prologterm.addAllowedState(zuweisung);
-          */
+        // define the NEA Automat
+        NeaState konstante = new NeaState("Konstante")  {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                return token.matches("[[a-z]{1}[a-zA-Z0-9_]*|[0-9]+]");
+            }
+        };
+
+        NeaState variable = new NeaState("Variable") {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                return token.matches("[A-Z_][a-zA-Z0-9_]*");
+            }
+        };
+
+        NeaState struktur = new NeaState("Struktur") {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                if(!token.matches("[a-z_]+\\(.+")) {
+                    return false;
+                }
+
+                String containingToken = getContainingToken(token, '(',')');
+                if(containingToken == null) {
+                    return false;
+                }
+
+                return parseWithOneOfFollowingStatesLeft(containingToken);
+            }
+        };
+
+        NeaState anweisung = new NeaState("Anweisung") {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                for(String subToken : splitListSave(token, ",", 0)) {
+                    if(parseWithOneOfFollowingStatesLeft(subToken)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        };
+
+        NeaState liste = new NeaState("Liste") {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                if(!token.matches("\\[.*\\]")) {
+                    return false;
+                }
+
+                if(token.equals("[]")) {
+                    return true; // empty list
+                }
+
+                return parseWithOneOfFollowingStatesLeft(token.substring(1, token.length() - 1));
+            }
+        };
+
+        NeaState zuweisung = new NeaState("Zuweisung") {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                if(!token.matches(".*:-.*")) {
+                    return false;
+                }
+
+                List<String> parts = splitListSave(token, ":-", 0);
+                if(parts.size() != 2) {
+                    return false;
+                }
+
+                return parseWithOneOfFollowingStatesLeft(parts.get(0)) && parseWithOneOfFollowingStatesRight(parts.get(1));
+            }
+        };
+
+        NeaState term = new NeaState("Term") {
+            @Override
+            public boolean parseToken(String token) throws ParseException {
+                if(!token.matches(".*\\.")) {
+                    return false;
+                }
+
+                return parseWithOneOfFollowingStatesLeft(token.substring(0, token.length() - 1));
+            }
+        };
+
+
+        struktur.addValidFollingStateOneLeft(anweisung);
+        anweisung.addValidFollingStateOneLeft(variable);
+        anweisung.addValidFollingStateOneLeft(konstante);
+        anweisung.addValidFollingStateOneLeft(struktur);
+        anweisung.addValidFollingStateOneLeft(liste);
+        liste.addValidFollingStateOneLeft(anweisung);
+        zuweisung.addValidFollingStateOneLeft(struktur);
+        zuweisung.addValidFollingStateOneLeft(variable);
+        zuweisung.addValidFollingStateOneLeft(konstante);
+        zuweisung.addValidFollingStateOneRight(anweisung);
+        term.addValidFollingStateOneLeft(struktur);
+        term.addValidFollingStateOneLeft(zuweisung);
+
+
         String tokens[] = entireFile.split("\\.");
 
-        //prologterm.parseToken(tokens[0]);
+
+        for(int i = 0; i < tokens.length; i++) {
+            try
+            {
+                Boolean result = term.parseToken(tokens[i].trim() + ".");
+                System.out.print("Result " + i + " for " + tokens[i] + " -> ");
+                System.out.println(result);
+            }
+            catch (ParseException ex) {
+                System.out.println("Result " + i + " for " + tokens[i] + " -> Exception:");
+                ex.printStackTrace();
+            }
 
 
+        }
 	}
 
 }
